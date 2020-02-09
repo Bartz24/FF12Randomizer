afe1f1e386c6279d9385298f799e35d3351fb2f6
Bartz24 <bartz24games@gmail.com>
2020-02-09 10:38:20 -0500

    new ui

---
+++ FF12Data/Data/Values/ValueUInt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FF12Data
+{
+    public static class ValueUInt
+    {
+        public static uint ReadUInt(this ByteArray data, int index)
+        {
+            return BitConverter.ToUInt32(data.Data, index);
+        }
+
+        public static void SetUInt(this ByteArray data, int index, uint value)
+        {
+            data.SetSubArray(index, BitConverter.GetBytes(value));
+        }
+    }
+}
---
+++ FF12Randomizer/Flags/Flag.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FF12Randomizer
+{
+    public abstract class Flag
+    {
+        public bool Enabled { get; set; }
+        public string Name { get; set; }
+
+        public static readonly Flag EMPTY = new SimpleFlag();
+    }
+}
